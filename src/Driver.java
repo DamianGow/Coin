fba0580584cca58635e72dd6e100c32d971b7c68
Damian Gow <damiangow38@gmail.com>
2024-01-16 19:22:34 -0500

    Refactor to shorten method.

--- src/Driver.java
+++ src/Driver.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 public class Driver {
 	   static Scanner scnr = new Scanner(System.in);
 	   public static void main(String[] args) {
-		   String coinResults = "";
-
-		   
 		   try {
-		   System.out.println("Your current coin is silver, input 'y' to switch, otherwise, input anything else");
+		   System.out.println("Your current coin is silver, input 'y' to switch, otherwise, input a number of times to flip");
 		   String switchChoice = scnr.next();
 		   if (switchChoice.equals("y")) {
 			   Game.switchCoin();
@@ -16,12 +13,10 @@ public class Driver {
 		   else if (switchChoice.equals("Easter")) {
 			   Game.secret();
 		   }
-		   
-		   System.out.println("Enter how many coinflips you would like to do, choosing 1 will show an image.");
-		   int flipTotal = flip();
-		 
-		   
-		   Game.flipMultiple(flipTotal);
+		   else {
+			   int flipTotal = flip();
+			   Game.flipMultiple(flipTotal);
+		   }
 		   } catch (InputMismatchException e) {
 			   System.out.println("You inputted something incorrectly, please try again");
 		   }
